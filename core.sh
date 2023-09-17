7549d2d286dc23e44f4f5e3e1157b82b7a54f11c
Daniella Mesquita <mesquitadaniella@proton.me>
2023-09-17 01:26:57 -0300

    Implement basic OS detection

--- core.sh
+++ core.sh
@@ -3,6 +3,76 @@
 username=$(logname)
 #export flouser #not needed yet
 
+# draft distro detection:
+if [ $(awk -F= '/^NAME/{print $2}' /etc/os-release | tr -d '"') == "Linux Mint" ]; then echo "Linux Mint"; fi
+if [ $(awk -F= '/^NAME/{print $2}' /etc/os-release | tr -d '"') == "Ubuntu" ]; then echo "Ubuntu"; fi
+if [ -e /usr/share/plymouth/themes/ubuntucinnamon-spinner ]; then echo "Ubuntu Cinnamon"; fi #this have to be placed exactly after Ubuntu, to patch the current value, bc UCR is also calling itself Ubuntu in /etc/os-release
+if [ $(awk -F= '/^NAME/{print $2}' /etc/os-release | tr -d '"') == "Floflis" ]; then echo "Floflis"; fi
+
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+BACKTITLE="Backtitle here"
+TITLE="Title here"
+MENU="How you prefer your mouse cursor color?"
+
+OPTIONS=(1 "⚫Black (like in \"MecOS\"🍎)"
+         2 "⚪White (like in \"WindOS\"🪟)")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            gsettings set org.cinnamon.desktop.interface cursor-theme 'Floflis' #from https://askubuntu.com/a/72093
+            ;;
+        2)
+            gsettings set org.cinnamon.desktop.interface cursor-theme 'DMZ-White' #from https://askubuntu.com/a/72093
+            ;;
+esac
+#from https://askubuntu.com/a/666179
+#in UI, will have different background as example
+
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+BACKTITLE="Backtitle here"
+TITLE="Title here"
+MENU="How you prefer your system theme?"
+
+OPTIONS=(1 "⚫Dark (let's save my eyes while computing in the dark)"
+         2 "🟤Normal (i have strong eyes)"
+         3 "⚪Light (i have stronger eyes, still; let's not abuse)")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            gsettings set org.cinnamon.desktop.wm.preferences theme 'Yaru-floflis-dark' && gsettings set org.cinnamon.desktop.interface gtk-theme 'Yaru-floflis-dark' && gsettings set org.cinnamon.theme name 'Yaru-floflis-dark' #from https://askubuntu.com/a/72093
+            ;;
+        2)
+            gsettings set org.cinnamon.desktop.wm.preferences theme 'Yaru-floflis' && gsettings set org.cinnamon.desktop.interface gtk-theme 'Yaru-floflis' && gsettings set org.cinnamon.theme name 'Yaru-floflis' #from https://askubuntu.com/a/72093
+            ;;
+        3)
+            gsettings set org.cinnamon.desktop.wm.preferences theme 'Yaru-floflis-light' && gsettings set org.cinnamon.desktop.interface gtk-theme 'Yaru-floflis-light' && gsettings set org.cinnamon.theme name 'Yaru-floflis-light' #from https://askubuntu.com/a/72093
+            ;;
+esac
+#from https://askubuntu.com/a/666179
+#in UI, will have different background as example
+
 #if [ -e /1 ]; then #only run what's inside if Floflis-based OS is detected
 #fi
 
@@ -45,67 +115,3 @@ esac
 #if user is an IT technician installing for a customer, don't ask and install MS Edge and Calculator right away)
 #floflis fixer should support reinstalling default calculator
 fi
-
-HEIGHT=15
-WIDTH=40
-CHOICE_HEIGHT=4
-BACKTITLE="Backtitle here"
-TITLE="Title here"
-MENU="How you prefer your mouse cursor color?"
-
-OPTIONS=(1 "⚫Black (like in \"MecOS\"🍎)"
-         2 "⚪White (like in \"WindOS\"🪟)")
-
-CHOICE=$(dialog --clear \
-                --backtitle "$BACKTITLE" \
-                --title "$TITLE" \
-                --menu "$MENU" \
-                $HEIGHT $WIDTH $CHOICE_HEIGHT \
-                "${OPTIONS[@]}" \
-                2>&1 >/dev/tty)
-
-clear
-case $CHOICE in
-        1)
-            gsettings set org.cinnamon.desktop.interface cursor-theme 'DMZ-White' #from https://askubuntu.com/a/72093
-            ;;
-        2)
-            gsettings set org.cinnamon.desktop.interface cursor-theme 'Floflis' #from https://askubuntu.com/a/72093
-            ;;
-esac
-#from https://askubuntu.com/a/666179
-#in UI, will have different background as example
-
-HEIGHT=15
-WIDTH=40
-CHOICE_HEIGHT=4
-BACKTITLE="Backtitle here"
-TITLE="Title here"
-MENU="How you prefer your system theme?"
-
-OPTIONS=(1 "⚫Dark (let's save my eyes while computing in the dark)"
-         2 "🟤Normal (i have strong eyes)"
-         3 "⚪Light (i have stronger eyes, still; let's not abuse)")
-
-CHOICE=$(dialog --clear \
-                --backtitle "$BACKTITLE" \
-                --title "$TITLE" \
-                --menu "$MENU" \
-                $HEIGHT $WIDTH $CHOICE_HEIGHT \
-                "${OPTIONS[@]}" \
-                2>&1 >/dev/tty)
-
-clear
-case $CHOICE in
-        1)
-            gsettings set org.cinnamon.desktop.wm.preferences theme 'Yaru-floflis-dark' && gsettings set org.cinnamon.desktop.interface gtk-theme 'Yaru-floflis-dark' && gsettings set org.cinnamon.theme name 'Yaru-floflis-dark' #from https://askubuntu.com/a/72093
-            ;;
-        2)
-            gsettings set org.cinnamon.desktop.wm.preferences theme 'Yaru-floflis' && gsettings set org.cinnamon.desktop.interface gtk-theme 'Yaru-floflis' && gsettings set org.cinnamon.theme name 'Yaru-floflis' #from https://askubuntu.com/a/72093
-            ;;
-        3)
-            gsettings set org.cinnamon.desktop.wm.preferences theme 'Yaru-floflis-light' && gsettings set org.cinnamon.desktop.interface gtk-theme 'Yaru-floflis-light' && gsettings set org.cinnamon.theme name 'Yaru-floflis-light' #from https://askubuntu.com/a/72093
-            ;;
-esac
-#from https://askubuntu.com/a/666179
-#in UI, will have different background as example
